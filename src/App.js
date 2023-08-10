32b8e5c1ae5b605106f6bc0a25b5b3e9ae15a9d3
honganh99 <130650517+honganh99@users.noreply.github.com>
2023-08-10 10:34:22 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import WordCounter from "./component/WordCounter";
+
+function App() {
+  return (
+    <div className="App">
+      <WordCounter />
+    </div>
+  );
+}
+
+export default App;
